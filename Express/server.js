ed2bdd864e94ddba6f5b8bd012835dc293966ded
Mukul Kumar Sahu <MukulKumar.Sahu@triconinfotech.com>
2024-11-20 17:24:34 +0530

    Updated Port

--- Express/server.js
+++ Express/server.js
@@ -111,7 +111,7 @@ app.post("/login", async (req, res) => {
   }
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
